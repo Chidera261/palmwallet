80ea8ac1543fa6bbb5ce93a7522f02d11a8b0b1d
Chidera261 <123710244+Chidera261@users.noreply.github.com>
2024-10-26 21:11:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Telegraf = require('telegraf');
+const { Telegraf } = require('telegraf'); // Update this line
 const bot = new Telegraf(process.env.YOUR_BOT_TOKEN); // Use the environment variable
 
 bot.start((ctx) => ctx.reply('Hello!'));
