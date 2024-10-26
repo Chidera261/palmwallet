63b364631bdef5bc6a30081398c3d6cf2c3ce49d
Chidera261 <123710244+Chidera261@users.noreply.github.com>
2024-10-26 21:30:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,24 @@
-const { Telegraf } = require('telegraf'); // Update this line
+const { Telegraf, Markup } = require('telegraf');
+
+// Replace with your actual bot token
 const bot = new Telegraf(process.env.YOUR_BOT_TOKEN); // Use the environment variable
 
-bot.start((ctx) => ctx.reply('Hello!'));
+// Start command handler
+bot.start((ctx) => {
+  ctx.reply('Hello! Please choose an option:', Markup.inlineKeyboard([
+    [Markup.button.callback('Option 1', 'option1')],
+    [Markup.button.callback('Option 2', 'option2')]
+  ]));
+});
 
+// Callback query handler
+bot.action('option1', (ctx) => {
+  ctx.answerCbQuery('You chose Option 1!');
+});
+
+bot.action('option2', (ctx) => {
+  ctx.answerCbQuery('You chose Option 2!');
+});
+
+// Start the bot
 bot.launch();
